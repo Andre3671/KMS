b89da45318e3f4587cdebdc6bb95e33cd657a41d
Andre3671 <55064742+Andre3671@users.noreply.github.com>
2021-03-23 15:20:07 +0100

    added vuesax and a basic sidebar

--- kms/src/App.vue
+++ kms/src/App.vue
@@ -1,18 +1,62 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="wel to Your Vue.js App"/>
+     <vs-button @click="active=!active" color="primary" type="filled">Open Sidebar</vs-button>
+      <vs-sidebar parent="body" default-index="1"  color="primary" class="sidebarx" spacer v-model="active">
+
+      <div class="header-sidebar" slot="header">
+        <h4>
+          User: 2142
+        </h4>
+      </div>
+
+      <vs-divider icon="View orders" position="left">
+        User
+      </vs-divider>
+      
+      <vs-sidebar-item index="1">
+        All orders
+      </vs-sidebar-item>
+
+      <vs-sidebar-item index="2">
+        My Orders
+      </vs-sidebar-item>
+
+      <vs-divider icon="Orders" position="left">
+        User
+      </vs-divider>
+
+      <vs-sidebar-item index="3">
+        Create Order
+      </vs-sidebar-item>
+      <vs-sidebar-item index="4">
+        Export order
+      </vs-sidebar-item>
+      <vs-sidebar-item index="5" >
+        Import Order
+      </vs-sidebar-item>
+
+      <div class="footer-sidebar" slot="footer">
+        <vs-button icon="reply" color="danger" type="flat">log out</vs-button>
+        <vs-button icon="settings" color="primary" type="border"></vs-button>
+      </div>
+
+    </vs-sidebar>
+    <Mainpage msg="wel to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mainpage from './components/Mainpage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Mainpage
   }
+  ,
+  data:()=>({
+    active:false,
+  })
 }
 </script>
 
--- kms/src/main.js
+++ kms/src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import Vuesax from 'vuesax'
+import VueRouter from 'vue-router'
 Vue.config.productionTip = false
 
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax, {
+  // options here
+})
+Vue.use(VueRouter)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
