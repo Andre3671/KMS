69e92352f325187f026b5ba37d27ae184dd48129
Gulnavaz wali <Gulnavazwali1@hotmail.com>
2021-04-05 15:39:43 +0200

    added login and my orders

--- kms/src/App.vue
+++ kms/src/App.vue
@@ -17,19 +17,59 @@
           Import order
         </vs-navbar-item>
         <template #right>
-          <vs-button color="#fff" flat >Login</vs-button>
+          <vs-button color="#fff" flat @click="popup=!popup" >Login</vs-button>
         </template>
       </vs-navbar>
   <div>
       <router-view style="margin-top:3%"/>
   </div>
-     
+     <vs-dialog v-model="popup">
+        <template #header>
+          <h4 class="not-margin">
+            <b>Login</b>
+          </h4>
+        </template>
+
+
+        <div class="con-form">
+          <vs-input v-model="email" placeholder="Email">
+            <template #icon>
+              @
+            </template>
+          </vs-input>
+          <vs-input type="password" v-model="password" placeholder="Password">
+            <template #icon>
+              <i class='bx bxs-lock'></i>
+            </template>
+          </vs-input>
+          <div class="flex">
+            <vs-checkbox v-model="remember">Remember me</vs-checkbox>
+            <a href="#">Forgot Password?</a>
+          </div>
+        </div>
+
+        <template #footer>
+          <div class="footer-dialog">
+            <vs-button block>
+              Sign In
+            </vs-button>
+
+            <div class="new">
+              New Here? <a href="#">Create New Account</a>
+            </div>
+          </div>
+        </template>
+      </vs-dialog>
   </div>
 </template>
   <script>
   export default {
     data:() => ({
-      active: 0
+      active: 0,
+      popup: false,
+      email: '',
+      password: '',
+      remember: false
     }),
      methods:{
     Routerpush: function(nav){
@@ -38,3 +78,50 @@
     }
   }
   </script>
+
+  <style>
+  .not-margin
+    {
+      margin: 0px;
+      font-weight: normal;
+      padding:10px;
+    }
+  .con-form
+    {
+      width: 100%;
+    }
+  .footer-dialog
+    {
+      display:flex;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      width:calc(100%);
+    }
+  .new
+    {
+      margin: 0px;
+      margin-top: 20px;
+      padding: 0px;
+      font-size: .7rem;
+    }
+  .flex
+    { display: flex;
+      align-items: center;
+      justify-content: space-between;  
+    } 
+  .vs-checkbox-label
+    {
+      font-size: .8rem;
+    }
+  .vs-input-content
+    {
+      margin: 10px 0px;
+      width: calc(100%);
+    }
+  .vs-input
+    {
+      width: 100%;
+    }
+                
+  </style>
