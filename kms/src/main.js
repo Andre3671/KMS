cb9466be68bce22e4da32ff7b19cce871bad08f3
Andre3671 <55064742+Andre3671@users.noreply.github.com>
2021-04-23 14:24:42 +0200

    lade till api projectet och vuex till frontenden.

--- kms/src/main.js
+++ kms/src/main.js
@@ -5,10 +5,12 @@ import Vuesax from 'vuesax'
 Vue.config.productionTip = false
 import 'vuesax/dist/vuesax.css' //Vuesax styles
 import router from './router'
+import Vuex from 'vuex'
 Vue.use(Vuesax, {
   // options here
 })
 new Vue({
   router,
+  Vuex,
   render: h => h(App)
 }).$mount('#app')
--- kms/src/views/AddOrder.vue
+++ kms/src/views/AddOrder.vue
@@ -1,13 +1,17 @@
 <template>
 <div>
   <h1>Create Order</h1>
+  <vs-input v-model="value" type="text"  label="Order number:" />
+    <vs-input v-model="value" type="date" label="Hullnr:" />
+      <vs-input v-model="value" type="date" label="Imo:" />
   <vs-input v-model="value" type="text" label="Vessel name:" />
   <vs-input v-model="value" type="text" label="Shipyard:" />
   <vs-input v-model="value" type="text" label="Owner:" />
-  <vs-input v-model="value" type="date" label="Hullnr:" />
+
     <vs-input v-model="value" type="text" label="Cycle:" />
       <vs-input v-model="value" type="text" label="Parts:" />
-        <vs-input v-model="value" type="text" label="Specifications:" />
+      <label for="spec">Specifications:</label>
+        <textarea id="spec" />
   <vs-button>Create</vs-button>
 </div>
 </template>
